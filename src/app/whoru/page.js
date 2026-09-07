17ba7a39ef8ba0913df53cdd832861d35330129b
Baldbogus <ybh8088@gmail.com>
2026-09-07 12:43:52 +0900

    Update page.js

--- src/app/whoru/page.js
+++ src/app/whoru/page.js
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import TeamBadge from "./components/TeamBadge";
+import TeamBadge from "../components/TeamBadge";
 
 export default function Home() {
 
